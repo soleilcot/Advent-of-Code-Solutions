266050c5645f952818d573417558458a3a165764
soleilcot <56422836+soleilcot@users.noreply.github.com>
2021-11-01 13:18:27 -0500

    Create Day1_part1.js

---
+++ 2015/Day 1/Day1_part1.js
@@ -0,0 +1,23 @@
+const https = require('https');
+const fs = require('fs')
+
+main();
+
+function main(){
+    let sInput = readInput()
+    
+    var climbCount = (sInput.match(/\(/g) || []).length
+    var descendCount = (sInput.match(/\)/g) || []).length
+
+    console.log("Santa ends up on floor",String(climbCount - descendCount))
+}
+
+function readInput(){
+    try {
+        const data = fs.readFileSync('input.txt', 'utf8')
+        return data
+    }   
+    catch (err) {
+        console.error(err)
+    }
+}
