a97959062174d4b16ff72d7c8afb0f4b0ab15033
Soleil <soleilcotterell@gmail.com>
2023-07-12 00:35:38 -0500

    feat(Day 8): Part 1 solution

---
+++ 2015/Day 8/part1.js
@@ -0,0 +1,55 @@
+const readline = require('readline');
+const fs = require('fs');
+
+readInput((err, schema) => {
+  if (err) {
+    console.error('An error occurred:', err);
+    return;
+  }
+
+  let result = readFile();
+  console.log(result);
+});
+
+function stripQuotes(str) {
+  return str.replace(/^"|"$/g, '');
+}
+
+function replaceHexWithAscii(str) {
+  return str.replace(/\\x([0-9A-Fa-f]{2})/g, function (match, p1) {
+    return String.fromCharCode(parseInt(p1, 16));
+  });
+}
+
+function getNumInCode(str) {
+  return str.trim().length;
+}
+
+function getNumInMemory(str) {
+  str = stripQuotes(str);
+  return str.replace(/\\\\|\\"|\\x[0-9A-Fa-f]{2}/g, '?').length;
+}
+
+function readInput() {
+  const fileStream = fs.createReadStream('input.txt');
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  let numInCode = 0;
+  let numInMemory = 0;
+
+  rl.on('line', (line) => {
+    numInCode += getNumInCode(line);
+    numInMemory += getNumInMemory(line);
+  });
+
+  rl.on('close', () => {
+    console.log(numInCode - numInMemory);
+  });
+
+  rl.on('error', (err) => {
+    console.log(err);
+  });
+}
